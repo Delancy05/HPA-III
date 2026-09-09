45188b31d3d8e2249bd2921e2724b425f44ac641
Delancy05 <141501866+Delancy05@users.noreply.github.com>
2026-09-08 20:04:14 -0500

    Add files via upload

---
+++ Lab 2/LibroCalificacion/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LibroCalificacion
+{
+    internal class PruebaLibroCalificacion
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            LibroCalificacion MyLibro = new LibroCalificacion();
+            MyLibro.MostrarMensaje();
+        }
+    }
+}
+
---
+++ Lab 2/LibroCalificacion_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LibroCalificacion_2
+{
+    public class LibroPruebaCalificacion 
+    {
+        static void Main(string[] args)
+        {
+            MiLibroCalificaciones MyLibro = new MiLibroCalificaciones();
+            Console.WriteLine("Por favor  ingrese el nombre del curso: ");
+
+            string nombreDelCurso = Console.ReadLine();
+            Console.WriteLine();
+
+            MyLibro.MostrarMensaje(nombreDelCurso);
+        }
+    }
+}
