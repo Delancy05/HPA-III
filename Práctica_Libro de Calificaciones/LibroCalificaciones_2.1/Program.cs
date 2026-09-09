a2fe055f4f46707cfcb282dbc188e7f7779fad1b
Delancy05 <141501866+Delancy05@users.noreply.github.com>
2026-09-08 20:18:04 -0500

    Add files via upload

---
+++ Práctica_Libro de Calificaciones/LibroCalificaciones_2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Crea el objeto LibroCalificaciones e invoca a su método DeterminarPromedio.
+public class PruebalibroCalificaciones
+{
+    public static void Main(string[] args)
+    {
+        // crea el objeto milibroCalificaciones y
+        // pasa el nombre del curso al constructor
+        LibroCalificaciones milibroCalificaciones = new LibroCalificaciones(
+            "CS101 Introducción a la programación en C#");
+
+        milibroCalificaciones.MostrarMensaje(); // muestra el mensaje de bienvenida
+        milibroCalificaciones.DeterminaPromedioClase(); // encuentra el promedio de
+        // calificaciones
+    } // fin de Main
+} // fin de la clase PruebalibroCalificaciones
