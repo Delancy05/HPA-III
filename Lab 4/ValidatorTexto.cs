2c6af2bf6dbf83220167e9077ecbf5c0a58e767f
Chelsy Ríos <chelsyrios0@gmail.com>
2026-09-23 23:21:53 -0500

    Elimina el repositorio interno de Lab 4

---
+++ Lab 4/ValidatorTexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_4
+{
+    public class ValidatorTexto : IValidadorCampo
+    {
+        public string MensajeError { get; private set; } = string.Empty;
+
+        public bool EsValido(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                MensajeError = "El campo de texto no puede estar vacío.";
+                return false;
+            }
+            return true;
+        }
+    }
+}
